c6457053b9f04510b75445f852db20ee9673f12f
Dmitriy <demianko@mail.ru>
2017-09-22 17:18:23 +0300

    test task from SDC

--- src/main/java/ru/demianko/sdc/web/HelloController.java
+++ src/main/java/ru/demianko/sdc/web/HelloController.java
@@ -1,6 +1,9 @@
 package ru.demianko.sdc.web;
 
 import org.springframework.security.acls.model.NotFoundException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.demianko.sdc.UserTo;
 import ru.demianko.sdc.model.Role;
@@ -16,18 +19,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.support.SessionStatus;
 import ru.demianko.sdc.util.PasswordUtil;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Controller
+
 public class HelloController {
     @Autowired
     private UserService service;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String printWelcome(ModelMap model) {
         model.addAttribute("message", "Hello world!");
         return "hello";
+    }
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String root() {
+        return "redirect:hello";
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
@@ -64,5 +73,14 @@ public class HelloController {
         model.addAttribute("message1", "For registration please enter your login, email and password(more 5 symbols).");
 
         return "login";
+    }
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
     }
 }
--- src/main/webapp/WEB-INF/pages/hello.jsp
+++ src/main/webapp/WEB-INF/pages/hello.jsp
@@ -1,5 +1,8 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <body>
 	<h1>${message}</h1>
+	<a href="<c:url value="/logout" />">Logout</a>
 </body>
 </html>
